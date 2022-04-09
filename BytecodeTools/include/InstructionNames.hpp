8cc5f20f235f16c2a904be1e4e9248ae63b2c7e3
Christoph Neumann <christophneumann2005@gmail.com>
2022-04-09 13:54:03 +0200

    Map of instructions to strings

--- BytecodeTools/include/InstructionNames.hpp
+++ BytecodeTools/include/InstructionNames.hpp
@@ -1,6 +1,6 @@
 #pragma once
-//This files contains mappings from bytecode to assembly commands and back. This is used to avoid duplication of strings and to make sure both compiler and decompiler use
-//the exact same names
+// This files contains mappings from bytecode to assembly commands and back. This is used to avoid duplication of strings and to make sure both compiler and decompiler use
+// the exact same names
 
 #include <unordered_map>
 #include <string>
@@ -9,6 +9,64 @@
 
 namespace vwa::bc
 {
-    std::unordered_map<const std::string &, BcInstruction> strInstrMap; //This is supposed to be generated automatically and refer to the strings in the array below
-    std::array<std::string, LastInstr> instrStrMap;
+    constexpr std::array<const char *, LastInstr> instrStrMap{
+        "CallFunc",
+        "Break",
+        "Continue",
+        "Exit",
+        "JumpRel",
+        "JumpRelIfFalse",
+        "JumpRelIfTrue",
+        "JumpFuncRel",
+        "JumpFPtr",
+        "JumpFuncAbs",
+        "JumpFFI",
+        "Return",
+        "Push",
+        "Pop",
+        "Push64",
+        "Push8",
+        "Dup",
+        "ReadAbs",
+        "WriteAbs",
+        "ReadRel",
+        "WriteRel",
+        "ReadMember",
+        "AbsOf",
+        "AbsOfConst",
+        "AddI",
+        "SubI",
+        "MulI",
+        "DivI",
+        "ModI",
+        "PowerI",
+        "AddF",
+        "SubF",
+        "MulF",
+        "DivF",
+        "PowerF",
+        "NegF",
+        "FtoI",
+        "FtoC",
+        "ItoC",
+        "ItoF",
+        "CtoI",
+        "CtoF",
+        "And",
+        "Or",
+        "Not",
+        "GreaterThanF",
+        "LessThanF",
+        "GreaterThanOrEqualF",
+        "LessThanOrEqualF",
+        "EqualF",
+        "NotEqualF",
+        "GreaterThanI",
+        "LessThanI",
+        "GreaterThanOrEqualI",
+        "LessThanOrEqualI",
+        "EqualI",
+        "NotEqualI",
+    };
+    extern std::unordered_map<std::string, BcInstruction> strInstrMap;
 }
